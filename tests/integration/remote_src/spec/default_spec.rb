05dc369b6701a46064c15b0868b1926192554b42
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-08-16 11:48:13 +0900

    [documentation] explain purpose of integration test

--- tests/integration/remote_src/spec/default_spec.rb
+++ tests/integration/remote_src/spec/default_spec.rb
@@ -7,6 +7,9 @@ sleep 10 if ENV["JENKINS_HOME"]
 
 context "after provisioning finished" do
   describe server(:server1) do
+    # XXX this integration test is intended to test if ansible play finishes
+    # successfully. this example looks silly but is enough to test the play
+    # itself.
     it "should be able to ping client" do
       result = current_server.ssh_exec("echo -n OK")
       expect(result).to eq "OK"
