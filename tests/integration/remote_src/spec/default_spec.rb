fd8f7c2949ce3ef79f0a4ef18f5ed366c9091c16
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-08-16 11:43:57 +0900

    [bugfix] set remote_src to yes and add integration test
    fixes #3

---
+++ tests/integration/remote_src/spec/default_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+class ServiceNotReady < StandardError
+end
+
+sleep 10 if ENV["JENKINS_HOME"]
+
+context "after provisioning finished" do
+  describe server(:server1) do
+    it "should be able to ping client" do
+      result = current_server.ssh_exec("echo -n OK")
+      expect(result).to eq "OK"
+    end
+  end
+end
